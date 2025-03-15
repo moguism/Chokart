ae151cb01c9f8d5ca4fb2e3aaaa85fafd5250565
Mauricio Rosso <148863935+moguism@users.noreply.github.com>
2025-03-15 12:52:32 +0100

    Movimiento del coche😭

---
+++ game/KartMario/Assets/Scripts/KartController.cs
@@ -0,0 +1,295 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.Rendering.PostProcessing;
+using Cinemachine;
+
+public class KartController : MonoBehaviour
+{
+    // Para los efectos (el bloom sobre todo vaya)
+    private PostProcessVolume postVolume;
+    private PostProcessProfile postProfile;
+
+    public Transform kartModel;
+    public Transform kartNormal;
+    public Rigidbody sphere;
+
+    public List<ParticleSystem> primaryParticles = new List<ParticleSystem>();
+    public List<ParticleSystem> secondaryParticles = new List<ParticleSystem>();
+
+    float speed, currentSpeed;
+    float rotate, currentRotate;
+    int driftDirection;
+    float driftPower;
+    int driftMode = 0;
+    bool first, second, third;
+    Color c;
+
+    [Header("Bools")]
+    public bool drifting;
+
+    [Header("Parameters")]
+
+    public float acceleration = 30f;
+    public float steering = 80f;
+    public float gravity = 10f;
+    public LayerMask layerMask;
+
+    [Header("Model Parts")]
+
+    public Transform frontWheels;
+    public Transform backWheels;
+    public Transform steeringWheel;
+
+    [Header("Particles")]
+    public Transform wheelParticles;
+    public Transform flashParticles;
+    public Color[] turboColors;
+
+    void Start()
+    {
+        postVolume = Camera.main.GetComponent<PostProcessVolume>();
+        postProfile = postVolume.profile;
+
+        // Para asignar los distintos colores de las partículas
+        for (int i = 0; i < wheelParticles.GetChild(0).childCount; i++)
+        {
+            primaryParticles.Add(wheelParticles.GetChild(0).GetChild(i).GetComponent<ParticleSystem>());
+        }
+
+        for (int i = 0; i < wheelParticles.GetChild(1).childCount; i++)
+        {
+            primaryParticles.Add(wheelParticles.GetChild(1).GetChild(i).GetComponent<ParticleSystem>());
+        }
+
+        foreach(ParticleSystem p in flashParticles.GetComponentsInChildren<ParticleSystem>())
+        {
+            secondaryParticles.Add(p);
+        }
+    }
+
+    void Update()
+    {
+        /*if (Input.GetKeyDown(KeyCode.Space))
+        {
+            float time = Time.timeScale == 1 ? .2f : 1;
+            Time.timeScale = time;
+        }*/
+
+        // La colisión es la que se mueve y nosotros la seguimos (sinceramente npi de por qué todo dios lo hace así)
+        transform.position = sphere.transform.position - new Vector3(0, 0.4f, 0);
+
+        // Moverse palante (en el vídeo lo del else no viene pero es que si no es muy cutre)
+        if (Input.GetButton("Fire1"))
+        {
+            speed = acceleration;
+        }
+        else
+        {
+            speed = 0;
+        }
+
+        // Para girar el modelo a la izquierda o la derecha
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            int dir = Input.GetAxis("Horizontal") > 0 ? 1 : -1;
+            float amount = Mathf.Abs((Input.GetAxis("Horizontal")));
+            Steer(dir, amount);
+        }
+
+        // AY MI MADRE EL DERRAPE
+        if (Input.GetButtonDown("Jump") && !drifting)
+        {
+            // En el tutorial no viene, pero yo quiero que pueda dar saltitos :(
+            if(Input.GetAxis("Horizontal") != 0 && speed != 0)
+            {
+                drifting = true;
+                driftDirection = Input.GetAxis("Horizontal") > 0 ? 1 : -1;
+
+                foreach (ParticleSystem p in primaryParticles)
+                {
+                    var main = p.main;
+                    main.startColor = Color.clear;
+                    p.Play();
+                }
+            }
+
+            kartModel.parent.DOComplete();
+            kartModel.parent.DOPunchPosition(transform.up * .2f, .3f, 5, 1);
+
+        }
+
+
+        if (drifting)
+        {
+            // El pavo del vídeo quería tener un rango entre 0 y 2 para controlar la fuerza del derrape
+            float control = (driftDirection == 1) ? ExtensionMethods.Remap(Input.GetAxis("Horizontal"), -1, 1, 0, 2) : ExtensionMethods.Remap(Input.GetAxis("Horizontal"), -1, 1, 2, 0);
+            float powerControl = (driftDirection == 1) ? ExtensionMethods.Remap(Input.GetAxis("Horizontal"), -1, 1, .2f, 1) : ExtensionMethods.Remap(Input.GetAxis("Horizontal"), -1, 1, 1, .2f);
+            Steer(driftDirection, control);
+            driftPower += powerControl;
+
+            ColorDrift();
+        }
+
+        if (Input.GetButtonUp("Jump") && drifting)
+        {
+            Boost();
+        }
+
+        // Para poder moverse y rotar, idk
+        currentSpeed = Mathf.SmoothStep(currentSpeed, speed, Time.deltaTime * 12f); speed = 0f;
+        currentRotate = Mathf.Lerp(currentRotate, rotate, Time.deltaTime * 4f); rotate = 0f;
+
+        // Para rotar el modelo con y sin derrape (qué coño es un Quaternion 😭)
+        if (!drifting)
+        {
+            kartModel.localEulerAngles = Vector3.Lerp(kartModel.localEulerAngles, new Vector3(0, 90 + (Input.GetAxis("Horizontal") * 15), kartModel.localEulerAngles.z), .2f);
+        }
+        else
+        {
+            float control = (driftDirection == 1) ? ExtensionMethods.Remap(Input.GetAxis("Horizontal"), -1, 1, .5f, 2) : ExtensionMethods.Remap(Input.GetAxis("Horizontal"), -1, 1, 2, .5f);
+            kartModel.parent.localRotation = Quaternion.Euler(0, Mathf.LerpAngle(kartModel.parent.localEulerAngles.y,(control * 15) * driftDirection, .2f), 0);
+        }
+
+        // Lo mismo pero con las ruedas y el volante
+        frontWheels.localEulerAngles = new Vector3(0, (Input.GetAxis("Horizontal") * 15), frontWheels.localEulerAngles.z);
+        frontWheels.localEulerAngles += new Vector3(0, 0, sphere.linearVelocity.magnitude/2);
+        backWheels.localEulerAngles += new Vector3(0, 0, sphere.linearVelocity.magnitude/2);
+
+        steeringWheel.localEulerAngles = new Vector3(-25, 90, ((Input.GetAxis("Horizontal") * 45)));
+
+    }
+
+    // FixedUpdate es como el _physics_process de Godot (se ejecuta cada cierto tiempo, siempre el mismo)
+    // Es la esfera la que hace todo
+    private void FixedUpdate()
+    {
+        // Aceleración, gravedad y rotación, respectivamente
+        if(!drifting)
+            sphere.AddForce(-kartModel.transform.right * currentSpeed, ForceMode.Acceleration);
+        else
+            sphere.AddForce(transform.forward * currentSpeed, ForceMode.Acceleration);
+
+        sphere.AddForce(Vector3.down * gravity, ForceMode.Acceleration);
+
+        transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, new Vector3(0, transform.eulerAngles.y + currentRotate, 0), Time.deltaTime * 5f);
+
+        // Para que si ha cochado con algo gire un poco el kart
+        RaycastHit hitOn;
+        RaycastHit hitNear;
+
+        Physics.Raycast(transform.position + (transform.up*.1f), Vector3.down, out hitOn, 1.1f,layerMask);
+        Physics.Raycast(transform.position + (transform.up * .1f)   , Vector3.down, out hitNear, 2.0f, layerMask);
+
+        kartNormal.up = Vector3.Lerp(kartNormal.up, hitNear.normal, Time.deltaTime * 8.0f);
+        kartNormal.Rotate(0, transform.eulerAngles.y, 0);
+    }
+
+    public void Boost()
+    {
+        drifting = false;
+
+        if (driftMode > 0)
+        {
+            DOVirtual.Float(currentSpeed * 3, currentSpeed, .3f * driftMode, Speed); // Para aumentar la velocidad
+            DOVirtual.Float(0, 1, .5f, ChromaticAmount).OnComplete(() => DOVirtual.Float(1, 0, .5f, ChromaticAmount)); // Dios como me encanta el bloom xD
+            kartModel.Find("Tube001").GetComponentInChildren<ParticleSystem>().Play(); // Tubo de escape (contaminación :c)
+            kartModel.Find("Tube002").GetComponentInChildren<ParticleSystem>().Play();
+        }
+
+        // Una vez que ha hecho toda la pesca pone todo a sus valores por defecto
+        driftPower = 0;
+        driftMode = 0;
+        first = false; second = false; third = false;
+
+        foreach (ParticleSystem p in primaryParticles)
+        {
+            var main = p.main;
+            main.startColor = Color.clear;
+            p.Stop();
+        }
+
+        kartModel.parent.DOLocalRotate(Vector3.zero, .5f).SetEase(Ease.OutBack);
+
+    }
+
+    // Para controlar cuánto tiene que girar en función de la "fuerza" del input
+    public void Steer(int direction, float amount)
+    {
+        rotate = (steering * direction) * amount;
+    }
+
+    // Para definir el color de las partículas
+    public void ColorDrift()
+    {
+        if(!first)
+            c = Color.clear;
+
+        if (driftPower > 50 && driftPower < 100-1 && !first)
+        {
+            first = true;
+            c = turboColors[0];
+            driftMode = 1;
+
+            PlayFlashParticle(c);
+        }
+
+        if (driftPower > 100 && driftPower < 150- 1 && !second)
+        {
+            second = true;
+            c = turboColors[1];
+            driftMode = 2;
+
+            PlayFlashParticle(c);
+        }
+
+        if (driftPower > 150 && !third)
+        {
+            third = true;
+            c = turboColors[2];
+            driftMode = 3;
+
+            PlayFlashParticle(c);
+        }
+
+        foreach (ParticleSystem p in primaryParticles)
+        {
+            var pmain = p.main;
+            pmain.startColor = c;
+        }
+
+        foreach(ParticleSystem p in secondaryParticles)
+        {
+            var pmain = p.main;
+            pmain.startColor = c;
+        }
+    }
+
+    void PlayFlashParticle(Color c)
+    {
+        GameObject.Find("CM vcam1").GetComponent<CinemachineImpulseSource>().GenerateImpulse();
+
+        foreach (ParticleSystem p in secondaryParticles)
+        {
+            var pmain = p.main;
+            pmain.startColor = c;
+            p.Play();
+        }
+    }
+
+    private void Speed(float x)
+    {
+        currentSpeed = x;
+    }
+
+    void ChromaticAmount(float x)
+    {
+        postProfile.GetSetting<ChromaticAberration>().intensity.value = x;
+    }
+
+    //private void OnDrawGizmos()
+    //{
+    //    Gizmos.color = Color.red;
+    //    Gizmos.DrawLine(transform.position + transform.up, transform.position - (transform.up * 2));
+    //}
+}
