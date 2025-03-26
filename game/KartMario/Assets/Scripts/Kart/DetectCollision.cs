460d983ea90ab7b36f43bde0aadfceb5e490c48a
moguism <mauriciorosso.aboy@gmail.com>
2025-03-26 12:58:40 +0100

    Spawn de objetos random y darse ostias (sin terminar)
    Quedan cositas por arreglar

--- game/KartMario/Assets/Scripts/Kart/DetectCollision.cs
+++ game/KartMario/Assets/Scripts/Kart/DetectCollision.cs
@@ -1,68 +1,103 @@
+using System.Linq;
+using Unity.Netcode;
 using UnityEngine;
 
-public class DetectCollision : MonoBehaviour
+public class DetectCollision : NetworkBehaviour
 {
     public KartController kart;
 
-    /*private void Start()
-    {
-        kart = transform.parent.GetComponentInChildren<KartController>();
-        print(kart);
-    }*/
-
+    // Seguramente haya que cambiar el DamageMultiplier porque la vida se va a quitar dos veces: el que choca (que pide que se quite vida) y el que recibe el choque (que calcula que tiene que restarse vida) 
     private void OnCollisionEnter(Collision collision)
     {
         var parent = collision.gameObject.transform.parent;
-        if (parent && parent.CompareTag("Enemy"))
+        if (parent && parent.CompareTag("Kart"))
         {
-            BasicPlayer basicPlayer = parent.GetComponent<BasicPlayer>(); // TODO: Habría que cambiar "BasicPlayer" por "KartController" (y quizás la forma a la que se accede al componente)
-            print("ENTER " + parent.name + ". Salud: " + basicPlayer.health);
+            KartController otherKart = parent.GetComponentInChildren<KartController>();
+            print("ENTER " + parent.name + ". Salud: " + otherKart.health);
 
             float kartSpeed = kart.sphere.linearVelocity.magnitude;
             print("VELOCIDAD PROPIA: " + kartSpeed);
 
-            /* TODO: DESCOMENTAR ESTO Y BORRAR LO QUE VIENE DESPUÉS CUANDO ESTÉ EL ONLINE
-        
             float otherKartSpeed = otherKart.sphere.linearVelocity.magnitude;
             print("VELOCIDAD DEL OPONENTE: " + otherKartSpeed);
 
             if (kartSpeed > otherKartSpeed)
             {
+                //Debug.LogError("a");
+
                 CheckAndRemoveObject(otherKart, kartSpeed);
             }
             else if (otherKartSpeed > kartSpeed)
             {
+                //Debug.LogError("b");
+
                 CheckAndRemoveObject(kart, otherKartSpeed);
             }
             else
             {
+                //Debug.LogError("c");
+
                 // Quito vida a los dos por igual
                 CheckAndRemoveObject(kart, otherKartSpeed);
                 CheckAndRemoveObject(otherKart, kartSpeed);
             }
-            */
-
-            basicPlayer.health -= kartSpeed * BasicPlayer.damageMultiplier;
-            print("SALUD RESTANTE: " + basicPlayer.health);
-
-            if (basicPlayer.health <= 0)
-            {
-                Destroy(parent.gameObject);
-            }
         }
-        else if(collision.gameObject.CompareTag("Ground"))
+        else if (collision.gameObject.CompareTag("Ground"))
         {
             kart.isGrounded = true;
         }
     }
 
-    private static void CheckAndRemoveObject(KartController kartController, float speed)
+    private void CheckAndRemoveObject(KartController kartController, float speed)
     {
-        kartController.health -= speed * BasicPlayer.damageMultiplier;
-        if (kartController.health <= 0)
+        if (IsOwner)
         {
-            Destroy(kartController.gameObject);
+            //Debug.LogError("d");
+            /*kartController.health -= damage;
+            Debug.LogWarning("La nueva vida es: " + kartController.health + ". El id es: " + kartController.NetworkObjectId);
+            if (kartController.health <= 0)
+            {
+                kartController.gameObject.SetActive(false);
+            }
+            NotifyHealthChangedClientRpc(damage, kartController.NetworkObjectId);*/
+
+            var damage = speed * BasicPlayer.damageMultiplier;
+            NotifyServerAboutChangeServerRpc(kartController.NetworkObjectId, damage);
         }
+    }
+
+    [ClientRpc]
+    private void NotifyHealthChangedClientRpc(float damage, ulong kartId, ClientRpcParams clientRpcParams = default)
+    {
+        //Debug.LogError("E");
+        KartController kart = FindObjectsByType<KartController>(FindObjectsSortMode.None).FirstOrDefault(k => k.NetworkObjectId == kartId);
+        kart.health -= damage;
+        Debug.LogWarning("La nueva vida es: " + kart.health + ". El id es: " + kart.NetworkObjectId);
+        if (kart.health <= 0)
+        {
+            Destroy(kart.gameObject);
+        }
+
+    }
+
+    [ServerRpc]
+    public void NotifyServerAboutChangeServerRpc(ulong kartId, float damage, ServerRpcParams rpcParams = default)
+    {
+        //Debug.LogError("G");
+        PositionManager positionManager = GameObject.Find("Triggers").GetComponent<PositionManager>();
+
+        KartController kart = positionManager.karts.FirstOrDefault(k => k.NetworkObjectId == kartId);
+
+        var parameters = new ClientRpcParams
+        {
+            Send = new ClientRpcSendParams
+            {
+                TargetClientIds = new ulong[] { kart.OwnerClientId }
+            }
+        };
+
+        NotifyHealthChangedClientRpc(damage, kartId, parameters);
+
     }
 
     private void OnCollisionExit(Collision collision)
