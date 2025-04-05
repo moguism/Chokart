1a1f56751573cdb6f8aa557820756af912f5261f
moguism <mauriciorosso.aboy@gmail.com>
2025-04-05 19:19:18 +0200

    SISTEMA DE LOBBIES CON JUEGO ONLINE COMPLETO 😭
    Larga vida a Code Monkey :'D

--- game/KartMario/Assets/Scripts/Utilities/ProjectInstaller.cs
+++ game/KartMario/Assets/Scripts/Utilities/ProjectInstaller.cs
@@ -6,12 +6,8 @@ public class ProjectInstaller : MonoInstaller
     [SerializeField]
     private WebsocketSingleton websocketSingleton;
 
-    [SerializeField]
-    private HostManager hostManager;
-
     public override void InstallBindings()
     {
         Container.BindInstance(websocketSingleton);
-        Container.BindInstance(hostManager);
     }
 }
