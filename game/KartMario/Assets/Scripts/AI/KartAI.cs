048f2100d07a6956f7b364d3a6c520aef2f7a9a7
maria99r <maria99r@hotmail.com>
2025-04-07 21:06:49 +0200

    comienzos ia (NO HACER MERGE QUE ESTA ROTO TODAVIA)

--- game/KartMario/Assets/Scripts/AI/KartAI.cs
+++ game/KartMario/Assets/Scripts/AI/KartAI.cs
@@ -4,10 +4,15 @@ using UnityEngine.AI;
 
 public class KartAI : NetworkBehaviour
 {
-    public Transform destination;
+    public Transform destination; // proximo trigger al que va a ir
 
-    [SerializeField]
-    private NavMeshAgent agent;
+    public float steerSensitivity = 1.0f;
+
+    public float HorizontalInput { get; private set; }
+    public int MoveDirection { get; private set; } 
+
+    /*[SerializeField]
+    private NavMeshAgent agent;*/
 
     [SerializeField]
     private SphereCollider sphereCollider;
@@ -17,24 +22,48 @@ public class KartAI : NetworkBehaviour
 
     public KartController enemyKart;
 
-    /*[SerializeField]
-    private KartController kart;*/
+    /*[SerializeField]*/
+    private KartController kart; // el coche
 
     public float speed = 2000;
 
+    private void Awake()
+    {
+        kart = GetComponent<KartController>();
+        Debug.Log("el coche es " , kart);
+    }
+        
     private void Start()
     {
         destination = FindFirstObjectByType<FinishLine>().transform;
+        Debug.Log("el destino de la ia es ", destination);
+    }
+
+    private void Update()
+    {
+        if (!destination || !kart) return;
+
+        Vector3 localTarget = kart.transform.InverseTransformPoint(destination.position);
+        HorizontalInput = Mathf.Clamp(localTarget.x, -1f, 1f) * steerSensitivity;
+
+        MoveDirection = (localTarget.z > 0.5f) ? 1 : (localTarget.z < -0.5f ? -1 : 0);
+
+        if (parent != null)
+        {
+            parent.horizontalInput = HorizontalInput;
+            parent.direction = MoveDirection;
+        }
     }
 
     // Básicamente el comportamiento es que en cuanto algún coche entra en su rango de visión, le persigue hasta que acaba con él, usando objetos también
     // Una vez acaba con él, va hacia el siguiente trigger del mapa
     private void LateUpdate()
     {
-        if(!IsOwner)
+        if (!IsOwner)
         {
+            Debug.LogError("NO ES DUEÑO DE SU PROPIO COCHE");
             return;
-        }
+        }/*
 
         parent.sphere.isKinematic = true;
 
@@ -51,6 +80,6 @@ public class KartAI : NetworkBehaviour
         agent.destination = destination.position;
         parent.sphere.transform.position = agent.transform.position;
 
-        parent.sphere.isKinematic = false;
+        parent.sphere.isKinematic = false;*/
     }
 }
