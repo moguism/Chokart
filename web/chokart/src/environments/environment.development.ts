00718ddc20662629cb30370246abfc7e88894377
maria99r <maria99r@hotmail.com>
2025-04-22 19:16:40 +0200

    login work

--- web/chokart/src/environments/environment.development.ts
+++ web/chokart/src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    apiUrl: "https://localhost:7048/api",
-    apiImg: "https://localhost:7048/",
+  apiUrl: 'https://localhost:7048/api/',
+  apiImg: 'https://localhost:7048/',
 };
